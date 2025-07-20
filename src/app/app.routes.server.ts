70550ad1b7ae9cbf92365e9514dd7a4502fdd43b
Kareem-Mohamed-Farouh <kimofro@gmail.com>
2025-07-20 21:53:07 +0300

    edite in rerender mode for dinamic component

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productdetails/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
